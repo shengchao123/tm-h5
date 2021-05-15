ac67ee556107317dcf786216080fbe3925cb992a
wangdongyao <mlyz.wdy@qq.com>
2021-05-15 14:30:06 +0800

    【fix】修改组件

--- src/pages/tabbar-home/index/components/NumList.vue
+++ src/pages/tabbar-home/index/components/NumList.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class='num-list-wrap'>
+  <div class='num-list-wrap center-align between-row'>
+    <div class="flex1 center-align">
+      <div class="column center-align">
+        <div class="line"></div>
+        <div class="circle"></div>
+        <div class="line"></div>
+      </div>
+
+      <div class="ft32 ml16">{{config.item.name}}</div>
+    </div>
+
+    <div class="center-align">
+      <div class="btn center ml8"
+           :style="{borderColor: config.item.isPlayed ? '#FF9204' : '#dcdcdc'}"
+           @click="onPlayer(config.item, index)">
+        <SvgIcon :icon="config.item.isPlayed ? 'icon_zanting': 'icon_bofang'"
+                 :style="{color: '#FF9204'}"
+                 class="icon"></SvgIcon>
+        <span class="ml4 color-666">语音</span>
+      </div>
+
+      <div class="btn center ml8"
+           @click="onGuideVisit">
+        <SvgIcon icon="icon_daolan"
+                 style="color: #4CBF00"
+                 class="icon"></SvgIcon>
+        <span class="ml4 color-666">导览</span>
+      </div>
+
+      <div class="btn center ml8"
+           @click="onGuide">
+        <SvgIcon icon="icon_daohang"
+                 style="color: #518CFC "
+                 class="icon"></SvgIcon>
+        <span class="ml4 color-666">导航</span>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -7,18 +43,71 @@
 <script>
 export default {
   name: 'NumList',
+  methods: {
+    onPlayer (item, index) {
+      const { isPlayed, mp3 } = item
+      this.pagePoints.forEach(point => {
+        point.isPlayed = false
+        if (point.mp3) point.mp3.pause()
+        return point
+      })
+      if (isPlayed && mp3) {
+        item.mp3.pause()
+        item.isPlayed = false
+      } else {
+        const mp3 = new Audio(item.url)
+        mp3.play() // 播放 mp3这个音频对象
+        item.isPlayed = true
+        item.mp3 = mp3
+      }
+
+      const _points = [...this.pagePoints]
+      _points[index] = item
+      this.pagePoints = _points
+    },
+    onGuideVisit () {
+
+    },
+    onGuide () {
+
+    }
+  },
   data () {
     return {
 
     }
   },
   props: {
-    title
+    config: Object
   }
 }
 </script>
 
 <style lang='scss' scoped>
 .num-list-wrap {
+  padding: 0 32rpx;
+  .line {
+    border-left: 1px solid #ffd53d;
+    width: 1px;
+    height: 40px;
+  }
+  .circle {
+    background: #feffff;
+    border: 1px solid #e1b406;
+    height: 40rpx;
+    width: 40rpx;
+    border-radius: 22rpx;
+    color: #e1b406;
+  }
+  .btn {
+    border: 1px solid #dcdcdc;
+    border-radius: 28rpx;
+    width: 120rpx;
+    height: 56rpx;
+    font-size: 26rpx;
+    .icon {
+      font-size: 16rpx;
+    }
+  }
 }
 </style>
--- src/pages/tabbar-home/index/components/ScenicSpot.vue
+++ src/pages/tabbar-home/index/components/ScenicSpot.vue
@@ -1,75 +1,20 @@
 <template>
-  <div class='scenic-spot-wrap pl12 mt16 flex1'>
-
-    <u-steps :list="numList"
-             :current="1"></u-steps>
-
-    <!-- <div slot="finish-icon"
-         class="finsih-icon bold center">{{index}}</div>
-    <div class="pb16 center-align">
-      <div class="ft18 bold flex1">{{item.name}}</div>
-
-      <div class="btn center ml8"
-           :style="{borderColor: item.isPlayed ? '#FF9204' : '#dcdcdc'}"
-           @click="onPlayer(item, index)">
-        <SvgIcon :icon="item.isPlayed ? 'icon_zanting': 'icon_bofang'"
-                 :style="{color: '#FF9204'}"
-                 class="icon"></SvgIcon>
-        <span class="ml4 ft16 color-666">语音</span>
-      </div>
-
-      <div class="btn center ml8"
-           @click="onGuideVisit">
-        <SvgIcon icon="icon_daolan"
-                 style="color: #4CBF00"
-                 class="icon"></SvgIcon>
-        <span class="ml4 ft16 color-666">导览</span>
-      </div>
-
-      <div class="btn center ml8"
-           @click="onGuide">
-        <SvgIcon icon="icon_daohang"
-                 style="color: #518CFC "
-                 class="icon"></SvgIcon>
-        <span class="ml4 ft16 color-666">导航</span>
-      </div> 
-   </div> -->
+  <div class='scenic-spot-wrap flex1'>
+    <NumList v-for="(item, index) in pagePoints"
+             :config="{item, index}"
+             :key="index"></NumList>
   </div>
 </template>
 
 <script>
+import NumList from '@/pages/tabbar-home/index/components/NumList'
 export default {
   name: 'ScenicSpot',
   methods: {
-    onPlayer (item, index) {
-      const { isPlayed, mp3 } = item
-      this.pagePoints.forEach(point => {
-        point.isPlayed = false
-        if (point.mp3) point.mp3.pause()
-        return point
-      })
-      if (isPlayed && mp3) {
-        item.mp3.pause()
-        item.isPlayed = false
-      } else {
-        const mp3 = new Audio(item.url)
-        mp3.play() // 播放 mp3这个音频对象
-        item.isPlayed = true
-        item.mp3 = mp3
-      }
 
-      const _points = [...this.pagePoints]
-      _points[index] = item
-      this.pagePoints = _points
-    },
-    onGuideVisit () {
-
-    },
-    onGuide () {
-
-    }
 
   },
+  components: { NumList },
   data () {
     return {
       pagePoints: []
@@ -94,25 +39,14 @@ export default {
 
 <style lang='scss' scoped>
 .scenic-spot-wrap {
+  position: absolute;
+  top: 264rpx;
+  bottom: 144rpx;
+  left: 0;
+  right: 0;
   overflow: scroll;
-  .finsih-icon {
-    background: #feffff;
-    border: 1px solid #e1b406;
-    height: 20rpx;
-    width: 20rpx;
-    border-radius: 12rpx;
-    font-size: 12rpx;
-    color: #e1b406;
-  }
-  .btn {
-    border: 1px solid #dcdcdc;
-    border-radius: 28rpx;
-    width: 60rpx;
-    height: 28rpx;
-    font-size: 16rpx;
-    .icon {
-      font-size: 8rpx;
-    }
-  }
+  box-sizing: border-box;
+  overflow: scroll;
+  white-space: nowrap;
 }
 </style>
