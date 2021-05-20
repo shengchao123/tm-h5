7c1f911833a3b0347d9042dc711b528b05fa1471
ylq <492502375@qq.com>
2021-05-20 14:54:42 +0800

    【feat】添加 webView

---
+++ src/pages/webView.vue
@@ -0,0 +1,20 @@
+<template>
+  <web-view :src="webUrl"></web-view>
+</template>
+<script>
+export default {
+  name: 'name',
+  data () {
+    return {
+      webUrl: ''
+    }
+  },
+  onLoad ({ webUrl }) {
+    this.webUrl = webUrl
+  },
+}
+</script>
+<style lang='scss' scoped>
+.web-view-wrap {
+}
+</style>
