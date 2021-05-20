5f89654de361ae43adcc597b36977b247c8c4d86
wangdongyao <mlyz.wdy@qq.com>
2021-05-20 18:47:38 +0800

    【fix】分段器

--- src/pages/league-interact/index/index.vue
+++ src/pages/league-interact/index/index.vue
@@ -1,18 +1,42 @@
 <template>
   <div class='league-interact-wrap'>
-
+    <div class="center mt32">
+      <div style="width:272rpx">
+        <u-subsection :list="subsection.list"
+                      height="56"
+                      mode="subsection"
+                      active-color="#E32417"
+                      :current="subsection.curNow"
+                      @change="subsectionChange"></u-subsection>
+      </div>
+    </div>
+    <Activity v-show="subsection.curNow === 1"></Activity>
   </div>
 </template>
 
 <script>
+import Activity from '@/pages/league-interact/Activity'
 export default {
   name: 'index',
   methods: {
+    subsectionChange () {
 
+    }
   },
+  components: { Activity },
   data () {
     return {
-
+      subsection: {
+        list: Object.freeze([
+          {
+            name: '论坛'
+          },
+          {
+            name: '活动'
+          }
+        ]),
+        curNow: 1
+      }
     }
   }
 }
