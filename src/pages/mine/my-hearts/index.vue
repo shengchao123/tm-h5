1112957323f5e24a355b4306acff7e22097e5261
shijing777 <sj18394180559@163.com>
2021-05-20 18:47:19 +0800

    【feat】暂存

---
+++ src/pages/mine/my-hearts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+    红四年
+
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
