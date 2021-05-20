a7beb9a4b0e2cf492d84bb11feab498d99cd037c
shijing777 <sj18394180559@163.com>
2021-05-20 18:09:38 +0800

    【feat】实名认证

--- src/pages/mine/published-agreement/index.vue
+++ src/pages/mine/published-agreement/index.vue
@@ -1,9 +1,8 @@
 <template>
   <view>
     <view class="border-b24 pt32 pl30 pr30">
-      <view class="color-333 ft32">查看协议</view>
-      <view class="ft30 color-666 mt24">这里是天目红盟论坛发表协议，正在努力编写中...... </view>
-
+      <view class="color-333 ft32 bold">查看协议</view>
+      <view class="ft30 color-666 mt24">{{content}}</view>
     </view>
   </view>
 </template>
@@ -13,7 +12,7 @@ export default {
   name: 'index',
   data () {
     return {
-
+      content: '这里是天目红盟论坛发表协议，正在努力编写中...... '
     }
   }
 }
