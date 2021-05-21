2151712ca892e3ab36d53c989ba381d1c4fcf211
ylq <492502375@qq.com>
2021-05-21 18:11:24 +0800

    【feat】风采详情 完善

--- src/pages/party/graphic/detail.vue
+++ src/pages/party/graphic/detail.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class='detail-wrap'>
+  <div class='detail-wrap'
+       v-if="baseInfo.orgId">
+    <video v-if="baseInfo.contentType === '04'"
+           class="video"
+           id="video"
+           :src="baseInfo.videoLinkUrl"
+           :enable-progress-gesture="true"
+           :auto-pause-if-navigate="true"
+           :page-gesture="true"
+           controls></video>
     <div class="title pt28 pl30 pr30">
       <div class="ft36 mb20 bold">{{baseInfo.title}}</div>
       <div class="ft26 color-666">
@@ -50,45 +59,12 @@ export default {
   },
   data () {
     return {
-      baseInfo: {
-        "content": "<p>地方撒旦发生分解啊法慢法门寺法慢发</p>",
-        "contentType": "03",
-        "contentTypeName": "",
-        orgName: '小阿三大苏打',
-        "digest": "发短发撒发顺丰安抚啊发放安抚啊发放啊啊啊",
-        "hyperlinksUrl": "",
-        "imageUrl": "",
-        "photoWallList": [
-          {
-            "description": "",
-            "imageUrl": "material/image/2021051819245642835059756182528.jpg"
-          },
-          {
-            "description": "",
-            "imageUrl": "material/image/2021051819245642835059756182528.jpg"
-          },
-          {
-            "description": "",
-            "imageUrl": "material/image/2021051819245642835059756182528.jpg"
-          },
-          {
-            "description": "",
-            "imageUrl": "material/image/2021051819245642835059756182528.jpg"
-          },
-          {
-            "description": "",
-            "imageUrl": "material/image/2021051819245642835059756182528.jpg"
-          }
-        ],
-        "title": "线上线下联动，学雷锋树新风",
-        "videoLinkUrl": "",
-        createTime: 1231414000000
-      }
+      baseInfo: {}
     }
   },
   onLoad (option) {
     this.id = option.id
-    // this.getGraphicInfoById()
+    this.getGraphicInfoById()
   }
 }
 </script>
@@ -101,6 +77,10 @@ page {
 <style lang='scss' scoped>
 .detail-wrap {
   color: #333;
+  .video {
+    width: 100%;
+    height: 560rpx;
+  }
   .type-01 {
     img {
       width: 100%;
