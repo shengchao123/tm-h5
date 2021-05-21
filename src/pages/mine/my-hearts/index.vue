c3650f17c5cd2ba4180156049d03395acf57a0a4
shijing777 <sj18394180559@163.com>
2021-05-21 14:14:09 +0800

    【feat】我的红心模块

--- src/pages/mine/my-hearts/index.vue
+++ src/pages/mine/my-hearts/index.vue
@@ -1,20 +1,125 @@
 <template>
   <view>
-    红四年
-
+    <view class="hearts-info">
+      <view class="center-align end-justify pt24 pr20 opacity-6">
+        <view @click="onPopup">
+          <svg-icon icon="icon_wenhao"
+                    class="ft24"></svg-icon>
+          <text class="ft22 ml8">红心获取规则</text>
+        </view>
+      </view>
+      <view class="column center-align mt20">
+        <text class="ft60 bold">{{heartsData.redHeartBalance}}</text>
+        <text class="ft24 opacity-6">当前红心值</text>
+      </view>
+      <view class="ft28 mt24 relative">
+        <view class="flex heart-accumulation">
+          <text class="opacity-6">获取</text>
+          <text>{{heartsData.redHeartAccumulation>0 ? '+' : ''}}{{heartsData.redHeartAccumulation}}</text>
+        </view>
+        <view class="flex heart-consume">
+          <text class="opacity-6">使用</text>
+          <text>{{heartsData.redHeartConsume > 0 ? '-' : ''}}{{heartsData.redHeartBalance}}</text>
+        </view>
+      </view>
+    </view>
+    <view class="pt4 pb4">
+      <u-tabs :list="tabsList"
+              :is-scroll="false"
+              font-size="28"
+              active-color="#E32417"
+              inactive-color="#666666"
+              bar-width="32"
+              bar-height="4"
+              :bar-style=tabsBarStyle
+              :current="tabsCurrent"
+              @change="changeTabs"></u-tabs>
+    </view>
+    <record :tabsCurrent="tabsCurrent"
+            ref="record"></record>
+    <access-rules ref="accessRules"></access-rules>
   </view>
 </template>
 
 <script>
+import AccessRules from './AccessRules.vue'
+import Record from './Record.vue'
 export default {
+  components: { AccessRules, Record },
   name: 'index',
+  methods: {
+    // 切换tab
+    changeTabs (index) {
+      this.tabsCurrent = index
+      this.$refs.record.resetGetList()
+    },
+    // 红心获取规则弹窗
+    onPopup () {
+      this.$refs.accessRules.show()
+    },
+    // 获取我的红心值
+    getMyRedHeart () {
+      this.$api.getMyRedHeart().then(res => {
+        if (res.isError) return this.$msg(res.message)
+        this.heartsData = res?.content ?? {}
+      })
+    }
+  },
   data () {
     return {
-
+      heartsData: {},
+      tabsList: [
+        {
+          name: '全部明细'
+        },
+        {
+          name: '获取'
+        },
+        {
+          name: '使用'
+        }
+      ],
+      tabsCurrent: 0
+    }
+  },
+  onShow () {
+    this.getMyRedHeart()
+  },
+  computed: {
+    tabsBarStyle () {
+      const temStyle = {
+        bottom: '6rpx'
+      }
+      return temStyle
     }
   }
 }
 </script>
 
 <style lang='scss' scoped>
+.opacity-6 {
+  opacity: 0.6;
+}
+.ft60 {
+  font-size: 60rpx;
+  line-height: 60rpx;
+}
+.end-justify {
+  justify-content: flex-end;
+}
+.hearts-info {
+  height: 262rpx;
+  width: 100%;
+  background: url("../../../static/hearts/top-bg.png") no-repeat;
+  background-size: 100% 100%;
+  color: #fff;
+  .heart-consume {
+    position: absolute;
+    left: calc(50% + 50rpx);
+  }
+  .heart-accumulation {
+    position: absolute;
+    right: calc(50% + 50rpx);
+  }
+}
 </style>
