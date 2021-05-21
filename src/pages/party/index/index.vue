eab125d8201deb11fa761c44a9119ddb0b4d735d
ylq <492502375@qq.com>
2021-05-21 19:29:00 +0800

    【feat】update

--- src/pages/party/index/index.vue
+++ src/pages/party/index/index.vue
@@ -8,9 +8,60 @@
         <div class="ft34 bold">{{baseInfo.name}}</div>
         <div class="tip ft20 ml16">{{baseInfo.regionsName}}</div>
       </div>
-      <div class="ft30 mt8">{{baseInfo.introduction}}</div>
+      <div class="ft30 mt8"
+           style="line-height: 42rpx">{{baseInfo.introduction}}</div>
     </div>
-    <org-tree class="mb20"></org-tree>
+    <div class="mb20 bg-white pb32">
+      <org-tree></org-tree>
+      <div class="center">
+        <div class="graphic-btn tc"
+             @click="onGraphic">风采展示</div>
+      </div>
+    </div>
+    <div class="bg-white pl30 pr30 mb20 pt24 pb30"
+         @click="onJourney">
+      <div class="center-align">
+        <svg-icon icon="icon_shifandai"
+                  class="ft32"
+                  style="color: #BF2B19"></svg-icon>
+        <span class="ft32 medium ml8">示范带</span>
+      </div>
+      <div class="between-row center-align pt28 pb24 pl40">
+        <span class="ft30 medium">{{baseInfo.journeyLineName}}</span>
+        <svg-icon icon="icon_xiangyoujiantou"
+                  class="ft20 color-999"></svg-icon>
+      </div>
+      <div class="flex"
+           style="color: #518CFC">
+        <svg-icon icon="icon_didian"
+                  class="ft24 mr14"></svg-icon>
+        <div class="flex1">{{pointsName}}</div>
+      </div>
+    </div>
+    <div class="bg-white pl30 pr30 mb20 pt24 pb30 pb48"
+         @click="onJourney">
+      <div class="center-align">
+        <svg-icon icon="icon_zhongdiangongzuo"
+                  class="ft32"
+                  style="color: #BF2B19"></svg-icon>
+        <span class="ft32 medium ml8">年度重点工作</span>
+      </div>
+      <div class="work-list">
+        <div v-for="(item, index) in baseInfo.keyWorkDTOList"
+             :key="index"
+             class="work-item">
+          <div class="icon-box mr14">
+            <svg-icon :icon="workStatusIcon(item).icon"
+                      class="ft24"
+                      :class="workStatusIcon(item).className"></svg-icon>
+          </div>
+          <div class="flex1">
+            <p class="ft30 mb16">{{item.content}}</p>
+            <p class="ft24 color-999">{{item.content}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <contact-person :list="baseInfo.addressBookList"></contact-person>
   </div>
 </template>
@@ -20,6 +71,17 @@ import ContactPerson from '../components/ContactPerson.vue'
 import OrgTree from './components/OrgTree.vue'
 export default {
   methods: {
+    onGraphic () {
+      uni.navigateTo({
+        url: '/pages/party/graphic/index'
+      })
+    },
+    onJourney () {
+      uni.$emit('initJourneyLineId', this.baseInfo.journeyLineId)
+      uni.switchTab({
+        url: '/pages/home/index/index'
+      })
+    },
     findOrgTreeByOrgId () {
       const params = {
         orgId: this.baseInfo.orgId
@@ -57,24 +119,59 @@ export default {
               "content": "xxx西安市擦拭擦拭的暗时大",
               "endDate": 12412341243154,
               "startDate": 5134134141233412,
-              "workStatus": 0,
+              "workStatus": 1,
+              "year": 2
+            },
+            {
+              "content": "xxx西安市擦拭擦拭的暗时大",
+              "endDate": 12412341243154,
+              "startDate": 5134134141233412,
+              "workStatus": 2,
+              "year": 2
+            },
+            {
+              "content": "xxx西安市擦拭擦拭的暗时大",
+              "endDate": 12412341243154,
+              "startDate": 5134134141233412,
+              "workStatus": 3,
               "year": 2
             }
           ],
           "lat": 0,
           "lng": 0,
           "name": "发的回复哈",
-          "pointNameList": [],
+          "pointNameList": ['石门老街', '新四军纪念馆', '攀岩小镇（龙上村）', '党史学'],
           "regionsCode": "",
           "regionsName": "大撒大"
         }
       ],
-      orgTree: []
+      orgTree: [],
+      workStatusMap: ''
     }
   },
   computed: {
     baseInfo () {
       return this.partyList[this.selectPoint] || {}
+    },
+    pointsName () {
+      return this.baseInfo.pointNameList.join('→')
+    },
+    workStatusIcon () {
+      return (item) => {
+        const { workStatus } = item
+        if (workStatus === 1) return {
+          className: 'color-999',
+          icon: 'icon_weikaishi'
+        }
+        if (workStatus === 2) return {
+          className: 'color-ffddb0',
+          icon: 'icon_jinhangzhong'
+        }
+        if (workStatus === 3) return {
+          className: 'color-green',
+          icon: 'icon_yiwancheng'
+        }
+      }
     }
   },
   onLoad () {
@@ -103,6 +200,29 @@ page {
       background-image: linear-gradient(98deg, #ffddb0 3%, #ffe6b0 72%);
       border-radius: 20rpx 0 20rpx 0;
       color: #bf2b19;
+    }
+  }
+  .graphic-btn {
+    width: 224rpx;
+    height: 56rpx;
+    line-height: 56rpx;
+    border-radius: 28rpx;
+    border: 1px solid #f54400;
+    color: #f54400;
+  }
+  .work-list {
+    .work-item {
+      .icon-box {
+        width: 24rpx;
+        height: 24rpx;
+        background: #fff;
+        .color-ffddb0 {
+          color: #ffddb0;
+        }
+        .color-green {
+          color: green;
+        }
+      }
     }
   }
 }
