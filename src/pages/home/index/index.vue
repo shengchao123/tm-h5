d340d6c01177d167eefd26bc224e4e8fa11eed8d
shengchao <1769620758@qq.com>
2021-05-22 21:19:51 +0800

    【feat】活动提醒样式更改

--- src/pages/home/index/index.vue
+++ src/pages/home/index/index.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class='tabbar-home-wrap'>
-    <Map needClick
-         :mapInitObj="mapInitObj"
-         mapClass="mapVH60"
-         :points="points"></Map>
+  <div class="tabbar-home-wrap">
+    <Map
+      needClick
+      :mapInitObj="mapInitObj"
+      mapClass="mapVH60"
+      :points="points"
+    ></Map>
     <DragPopover @dragTopChange="dragTopChange">
-      <mescroll-uni ref="mescrollRef"
-                    class="mescroll-wrap"
-                    :top="scrollTop"
-                    :up="upOption"
-                    @init="mescrollInit"
-                    @down="downCallback"
-                    @up="upCallback">
+      <mescroll-uni
+        ref="mescrollRef"
+        class="mescroll-wrap"
+        :top="scrollTop"
+        :up="upOption"
+        @init="mescrollInit"
+        @down="downCallback"
+        @up="upCallback"
+      >
         <template>
           <div class="box relative">
             <PathsList @onSelectPath="onSelectPath"></PathsList>
             <div class="content">
-              <ScenicSpot :points="points"
-                          ref="refScenicSpot"></ScenicSpot>
+              <ScenicSpot :points="points" ref="refScenicSpot"></ScenicSpot>
               <div class="center pb32 pt16 create-btn-wrap">
-                <div class="create-btn center bold"
-                     @click="onCreateTravel">创建我的行程</div>
+                <div class="create-btn center bold" @click="onCreateTravel">
+                  创建我的行程
+                </div>
               </div>
             </div>
             <activity-swiper></activity-swiper>
@@ -29,55 +33,56 @@
       </mescroll-uni>
     </DragPopover>
 
-  <btn-dialog></btn-dialog>
+    <btn-dialog></btn-dialog>
   </div>
 </template>
 
 <script>
-import Map from '@/pages/components/Map.vue'
-import DragPopover from '@/components/DragPopover'
-import PathsList from './components/PathsList'
-import ScenicSpot from './components/ScenicSpot'
-import BtnDialog from './components/BtnDialog'
-import ActivitySwiper from './components/ActivitySwiper'
+import Map from "@/pages/components/Map.vue";
+import DragPopover from "@/components/DragPopover";
+import PathsList from "./components/PathsList";
+import ScenicSpot from "./components/ScenicSpot";
+import BtnDialog from "./components/BtnDialog";
+import ActivitySwiper from "./components/ActivitySwiper";
 import MescrollMixin from "@/components/mescroll-uni/mescroll-mixins.js";
 import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue";
 export default {
   name: "index",
   methods: {
     // 下拉刷新
-    downCallback () {
+    downCallback() {
       this.mescroll.resetUpScroll(); // 重置列表为第一页
     },
     // 加载更多
-    upCallback (page) {
-      this.getItineraryEvaluationPage(page)
+    upCallback(page) {
+      this.getItineraryEvaluationPage(page);
     },
-    resetGetList () {
-      this.listData = []
-      this.mescroll.resetUpScroll()
+    resetGetList() {
+      this.listData = [];
+      this.mescroll.resetUpScroll();
     },
 
-    getItineraryEvaluationPage (page) {
+    getItineraryEvaluationPage(page) {
       const params = {
         pageNumber: page ? page.num : 1,
-        pageSize: page ? page.size : 10
-      }
-      this.$api.getItineraryEvaluationPage(params).then(res => {
-        if (res.isError) return this.mescroll.endErr()
-        const { items, count } = res.content
-        this.mescroll.endBySize(items.length, count)
-        const list = res.content.items
-        this.evaluationList = params.pageNumber === 1 ? list : this.evaluationList.concat(list)
-      })
+        pageSize: page ? page.size : 10,
+      };
+      this.$api.getItineraryEvaluationPage(params).then((res) => {
+        if (res.isError) return this.mescroll.endErr();
+        const { items, count } = res.content;
+        this.mescroll.endBySize(items.length, count);
+        const list = res.content.items;
+        this.evaluationList =
+          params.pageNumber === 1 ? list : this.evaluationList.concat(list);
+      });
     },
 
-    dragTopChange (top) {
-      this.scrollTop = (top * 2) + 32
+    dragTopChange(top) {
+      this.scrollTop = top * 2 + 32;
     },
 
-    onCreateTravel () {
-      uni.navigateTo({ url: '/pages/home/stroke-order/index' })
+    onCreateTravel() {
+      uni.navigateTo({ url: "/pages/home/stroke-order/index" });
     },
     onSelectPath(journeyLineId) {
       this.$nextTick(() => {
@@ -95,7 +100,7 @@ export default {
         if (res.isError) return;
         this.points = res.content;
       });
-    }
+    },
   },
   data() {
     return {
@@ -103,7 +108,7 @@ export default {
       mapInitObj: Object.freeze({
         resizeEnable: true,
         zoom: 9, // 级别
-        center: [119.365056, 30.034302]
+        center: [119.365056, 30.034302],
       }),
       upOption: {
         empty: {
@@ -113,8 +118,8 @@ export default {
         noMoreSize: 8, // 配置列表的总数量要大于等于1条才显示'-- END --'的提示
       },
       scrollTop: 630,
-      evaluationList: []
-    }
+      evaluationList: [],
+    };
   },
   components: {
     Map,
@@ -123,13 +128,14 @@ export default {
     ScenicSpot,
     BtnDialog,
     ActivitySwiper,
-    MescrollUni
+    MescrollUni,
   },
-  onLoad (option) {
-    if (option.masterOrgId) uni.setStorageSync('masterOrgId', option.masterOrgId)
+  onLoad(option) {
+    if (option.masterOrgId)
+      uni.setStorageSync("masterOrgId", option.masterOrgId);
   },
   mixins: [MescrollMixin],
-}
+};
 </script>
 
 <style lang="scss" scoped>
