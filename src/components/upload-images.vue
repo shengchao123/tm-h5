fb667f00ee1768535f2524aca4e78ccd523fce60
shengchao <1769620758@qq.com>
2021-05-24 17:14:59 +0800

    【fix】初心之旅发布评价样式修改；以及上传图片增加拍照选择图片；

--- src/components/upload-images.vue
+++ src/components/upload-images.vue
@@ -59,7 +59,7 @@ export default {
       uni.chooseImage({
         count: this.rduLength < this.count ? this.rduLength : this.count, //最多可以选择的图片张数，默认9
         sizeType: ['original', 'compressed'], //original 原图，compressed 压缩图，默认二者都有
-        sourceType: ['album'], //album 从相册选图，camera 使用相机，默认二者都有
+        sourceType: ['album', 'camera'], //album 从相册选图，camera 使用相机，默认二者都有
         success: (res) => {
           const images = res.tempFilePaths;
           this.uploadFiles(images);
