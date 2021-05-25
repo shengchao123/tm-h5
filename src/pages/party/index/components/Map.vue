9e06b99d5d49dc169b066bdec6a116f36698bf36
wangdongyao <mlyz.wdy@qq.com>
2021-05-25 15:16:58 +0800

    【fix】修改地图

--- src/pages/party/index/components/Map.vue
+++ src/pages/party/index/components/Map.vue
@@ -75,8 +75,6 @@ export default {
   data () {
     return {
       mapInitObj: Object.freeze({
-        dragEnable: false,
-        zoomEnable: false,
         resizeEnable: true,
         zoom: 9, // 级别
         zooms: [9, 19],
