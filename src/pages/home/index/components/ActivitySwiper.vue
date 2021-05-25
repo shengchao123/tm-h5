c34e8d159bf35116c81c673bac245d69f768fd0e
shengchao <1769620758@qq.com>
2021-05-25 16:19:08 +0800

    【fix】初心之旅的活动提醒样式更改；

--- src/pages/home/index/components/ActivitySwiper.vue
+++ src/pages/home/index/components/ActivitySwiper.vue
@@ -4,10 +4,12 @@
     <view class="pt30">
       <view class="between-row pl30 pr30">
         <view class="bold ft32">活动提醒</view>
-        <view v-if="activityList.length > 0">{{ current }}/{{ activityList.length }}</view>
+        <view v-if="activityList.length > 0"
+              class="color-999 ft24">{{ current }}/{{ activityList.length }}</view>
       </view>
       <view>
         <swiper class="swiper mt24"
+                :class="swiperClass"
                 :duration="1000"
                 :circular="true"
                 @change="getSwiper">
@@ -17,12 +19,12 @@
             <view class="activity-item mt16 ml30 mr30"
                   @click="onActivityDetail(item)">
               <view class="ft28 color-333 center-align">
-                <div> {{ item.name }}</div>
+                <div>{{ item.name }}</div>
                 <svg-icon :icon="activityStatus(item.status)"
                           :class="activityClass(item.status)"
                           class="ml8 activity-status"></svg-icon>
               </view>
-              <view class="between-row ft24 color-999">
+              <view class="between-row ft24 color-999 mt20">
                 <view>{{ item.orgName }}</view>
                 <view>{{ $moment(item.endTime).format("YYYY-MM-DD") }}</view>
               </view>
@@ -48,7 +50,7 @@ export default {
   data () {
     return {
       activityList: [],
-      current: 1,
+      current: 1
     };
   },
   methods: {
@@ -80,6 +82,14 @@ export default {
         return activityClass.get(status)
       }
     },
+    // 轮播样式
+    swiperClass () {
+      if (this.activityList.length === 1) {
+        return 'swiper-one'
+      } else {
+        return 'swipers'
+      }
+    }
   },
   created () {
     this.getActivityList();
@@ -97,15 +107,28 @@ export default {
   .swiper-item {
     height: 150rpx !important;
   }
+  .activity-item {
+    padding: 22rpx 24rpx;
+    background: #ffffff;
+    box-shadow: 3rpx 2rpx 12rpx 8rpx rgba(17, 17, 17, 0.03);
+    border-radius: 6rpx;
+    .activity-status {
+      width: 88rpx;
+      height: 32rpx;
+    }
+  }
 }
-.activity-item {
-  padding: 22rpx 24rpx;
-  background: #ffffff;
-  box-shadow: 3rpx 2rpx 12rpx 8rpx rgba(17, 17, 17, 0.03);
-  border-radius: 6rpx;
-  .activity-status {
-    width: 88rpx;
-    height: 32rpx;
+.swiper-one {
+  /deep/.uni-swiper-slide-frame {
+    width: 100% !important;
+  }
+}
+.swipers {
+  /deep/.uni-swiper-slide-frame {
+    width: 70% !important;
+  }
+  .activity-item {
+    width: 490rpx;
   }
 }
 .activity-in {
