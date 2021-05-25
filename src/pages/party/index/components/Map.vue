7ec43a007106288a3a2056304e5cfaa331af1ad2
wangdongyao <mlyz.wdy@qq.com>
2021-05-25 14:23:30 +0800

    【fix】地图显示

--- src/pages/party/index/components/Map.vue
+++ src/pages/party/index/components/Map.vue
@@ -17,11 +17,15 @@ export default {
     // 绘制图中标记点
     drawMarker () {
       const _temPoints = JSON.parse(JSON.stringify(this.points))
+
+      const _currentPoint = _temPoints.splice(this.currentIndex, 1)
+
+
       // 绘制图标
       _temPoints.forEach((item, index) => {
 
-        const _img = index === this.currentIndex ? 'party_l.gif' : 'party.png'
-        const _WH = index === this.currentIndex ? LWH : MWH
+        const _img = 'party.png'
+        const _WH = MWH
 
         const Icon = new AMap.Icon({
           size: new AMap.Size(_WH.W, _WH.H),
@@ -42,6 +46,32 @@ export default {
         // 点击方法绑定
         marker.on('click', this.markerClick)
       })
+
+      this.drawMainMark(_currentPoint)
+
+    },
+
+    drawMainMark (_currentPoint) {
+      const _img = 'party_l.png'
+      const _WH = LWH
+      const Icon = new AMap.Icon({
+        size: new AMap.Size(_WH.W, _WH.H),
+        image: require(`@/static/map/${_img}`),
+        imageSize: new AMap.Size(_WH.W, _WH.H)
+      })
+
+      // 绘制标记气球
+      const marker = new AMap.Marker({
+        position: new AMap.LngLat(item.lng, item.lat),
+        map: this.$amap,
+        offset: new AMap.Pixel(-_WH.W / 2, -_WH.H),
+        icon: Icon,
+        touchZoom: false
+      })
+      // 设置 marker 绑定的数据
+      marker.setExtData(index)
+      // 点击方法绑定
+      marker.on('click', this.markerClick)
     },
 
     markerClick (e) {
