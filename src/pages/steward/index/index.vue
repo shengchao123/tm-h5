1e04aeb8c240e2b16a6d53f4da8d48a37a69daed
ylq <492502375@qq.com>
2021-06-01 13:59:27 +0800

    【fix】update

--- src/pages/steward/index/index.vue
+++ src/pages/steward/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='steward-wrap'>
     <img src="@/static/test/11.png">
+    <custom-tabbar></custom-tabbar>
   </div>
 </template>
 
@@ -17,6 +18,7 @@ export default {
 
 <style lang='scss' scoped>
 .steward-wrap {
+  padding-bottom: 100rpx;
   img {
     width: 100vw;
   }
