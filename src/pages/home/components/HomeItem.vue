d88b81e04b9edae3bef8840f07bbaf75fb251d02
wangdongyao <mlyz.wdy@qq.com>
2021-06-04 19:03:41 +0800

    【fix】首页完成

---
+++ src/pages/home/components/HomeItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='item-wrap column center'>
+    <img :src="item.img"
+         :style="{width: item.imgW + 'rpx'}"
+         object-fit="contain" />
+    <div class="title mt16">{{item.text}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeItem',
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.item-wrap {
+  position: absolute;
+  background-image: linear-gradient(90deg, #ffdb5d 21%, #ffca00 96%);
+  border-radius: 8px;
+  width: 128rpx;
+  height: 178rpx;
+  .title {
+    font-family: PingFangSC-Regular;
+    font-size: 24rpx;
+    color: #333333;
+  }
+}
+</style>
