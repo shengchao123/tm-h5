fd4f7bd514b6864064bb08baa2a150e47e2992d6
ylq <492502375@qq.com>
2021-06-09 14:37:37 +0800

    【feat】地标简介字段变更

--- src/pages/original-travel/introduction/index.vue
+++ src/pages/original-travel/introduction/index.vue
@@ -167,10 +167,12 @@ export default {
       const { openDay, startTime, endTime } = this.baseInfo
       if (this.$isEmpty(openDay)) return ''
       const week = filterContinuousDate(openDay)
-      if (startTime === '00:00' && endTime === '23:59') {
+      const start = this.$moment(startTime).format('HH:mm')
+      const end = this.$moment(endTime).format('HH:mm')
+      if (start === '00:00' && end === '23:59') {
         return `整天(${week})，对外开放`
       }
-      return `${startTime}-${endTime}(${week})，对外开放`
+      return `${start}-${end}(${week})，对外开放`
     }
   },
   onLoad (option) {
