bc711470b4cc37c34fd5679fb686e6426cdeb3ed
shengchao <1769620758@qq.com>
2021-06-09 16:29:50 +0800

    【feat】留言页面

---
+++ src/pages/mine/leave-message/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>
+
+  </view>
+</template>
+<script>
+export default {
+  methods: {
+
+  },
+  data () {
+    return {
+    }
+  },
+  onShow () {
+    this.getListData()
+  },
+  created () { },
+  components: {},
+}
+</script>
+<style>
+page {
+  background: #f7f7f7;
+}
+</style>
+<style lang='scss' scoped>
+</style>
