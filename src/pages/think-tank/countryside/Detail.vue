da6bb81ddde317106a3184577065b6a2029fbb40
shengchao <1769620758@qq.com>
2021-06-18 16:14:14 +0800

    【feat】无感直补增加涉农接口；无感直补、和详情页面重写；

--- src/pages/think-tank/countryside/Detail.vue
+++ src/pages/think-tank/countryside/Detail.vue
@@ -1,6 +1,29 @@
 <template>
   <div class='detail-wrap'>
-    <img src="@/static/test/13.png">
+    <!-- <img src="@/static/test/13.png"> -->
+    <div class="ft40 bold mb24">{{inductiveDetail.title}}</div>
+    <div class="center-align ft26 color-666 mb24">
+      <div class="mr48">{{$moment(inductiveDetail.createTime).format('YYYY年MM月DD日')}}</div>
+      <div>{{inductiveDetail.publishingDepartment}}</div>
+    </div>
+    <div v-html="inductiveDetail.content"></div>
+    <div class="flex-wrap mt30">
+      <div v-for="(img,index) in inductiveDetail.picList"
+           :key="index">
+        <img :src="$fileHost + img.url"
+             alt=""
+             :class="(index + 1) % 3 === 0?'img':'img mr24'">
+      </div>
+    </div>
+    <div>
+      <div class="bold ft28 mb24 mt48">附件</div>
+      <div v-for="(item,index) in inductiveDetail.attachmentList"
+           :key="index"
+           class="ft24 attachment mb16">
+        <span>{{index + 1}}.</span>
+        <span>{{item.name}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,16 +32,40 @@ export default {
   name: 'Detail',
   data () {
     return {
-
+      inductiveDetail: {}
     }
+  },
+  methods: {
+    getAgricultureDetail (id) {
+      const params = {
+        id
+      }
+      this.$api.getAgricultureDetail(params).then(res => {
+        if (res.isError) return this.$msg(res.message)
+        this.inductiveDetail = res.content
+      })
+    }
+  },
+  onLoad (option) {
+    option.id && this.getAgricultureDetail(option.id)
   }
 }
 </script>
 
 <style lang='scss' scoped>
 .detail-wrap {
-  img {
-    width: 100vw;
+  padding: 24rpx 30rpx;
+  .img {
+    width: 214rpx;
+    height: 214rpx;
+    border-radius: 8rpx;
+  }
+  .attachment {
+    width: 686rpx;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: #518cfc;
   }
 }
 </style>
