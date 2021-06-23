9a2020444c6cdcfb64201891c19b9303e530f0d0
shengchao <1769620758@qq.com>
2021-06-23 16:21:54 +0800

    【fix】预览word的地址更改；

--- src/pages/think-tank/countryside/attachmentPage.vue
+++ src/pages/think-tank/countryside/attachmentPage.vue
@@ -4,8 +4,6 @@
       <iframe :src="fileUrl"
               width='100%'
               height='800rpx'
-              frameborder='no'
-              scrilling="no"
               class="iframe"
               v-if="!showPdf"></iframe>
       <div v-if="showPdf">
@@ -57,7 +55,7 @@ export default {
       const fileType = this.fileType
       const fileHost = this.$fileHost
       if (wordTypes.includes(fileType)) {
-        return 'https://view.officeapps.live.com/op/view.aspx?src=' + fileHost + file
+        return '//www.xdocin.com/xdoc?_func=to&_format=html&_cache=1&_xdoc=' + fileHost + file
       }
     },
     showPdf () {
