1f2752fee97c538e802f6240b8593811e00f80f4
ylq <492502375@qq.com>
2021-07-08 10:01:38 +0800

    【feat】好帮手 页面 入口添加

--- src/pages/steward/index/index.vue
+++ src/pages/steward/index/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class='steward-wrap'>
+  <div class='steward-wrap relative'>
     <img src="@/static/test/11.png"
          @click.self="onMessage">
+    <div class="help"
+         @click="onGoodHelper"></div>
     <custom-tabbar></custom-tabbar>
   </div>
 </template>
@@ -9,6 +11,11 @@
 <script>
 export default {
   methods: {
+    onGoodHelper () {
+      uni.navigateTo({
+        url: '/pages/steward/good-helper/index'
+      })
+    },
     onMessage () {
       this.$msg('功能开发中')
     }
@@ -19,6 +26,17 @@ export default {
 <style lang='scss' scoped>
 .steward-wrap {
   padding-bottom: 100rpx;
+  .help {
+    position: absolute;
+    top: -920rpx;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: 100rpx;
+    height: 150rpx;
+    z-index: 1;
+  }
   img {
     width: 100vw;
   }
